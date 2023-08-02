9e49b78d3c23f5369ee25bc31305184c2235773f
hardkill551 <hardkill194@gmail.com>
2023-08-02 20:35:37 -0300

    feat/hotelInformations

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -3,7 +3,32 @@ import { prisma } from "@/config";
 async function findHotels() {
   return prisma.hotel.findMany();
 }
-
+async function findHotel(hotelId:number) {
+  const hotelRooms = await prisma.room.aggregate({
+    _sum:{
+      capacity:true
+    },
+    where:{
+      hotelId
+    }
+  });
+  return hotelRooms._sum.capacity
+}
+async function findBooking(hotelId:number) {
+  const bookings = await prisma.room.findMany({
+    select:{
+      _count:{
+        select:{
+          Booking: true,
+        },
+      }
+    },
+    where:{
+      hotelId
+    }
+  });
+  return bookings
+}
 async function findRoomsByHotelId(hotelId: number) {
   return prisma.hotel.findFirst({
     where: {
@@ -17,7 +42,9 @@ async function findRoomsByHotelId(hotelId: number) {
 
 const hotelRepository = {
   findHotels,
+  findHotel,
   findRoomsByHotelId,
+  findBooking
 };
 
 export default hotelRepository;
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -3,6 +3,10 @@ import enrollmentRepository from "@/repositories/enrollment-repository";
 import ticketRepository from "@/repositories/ticket-repository";
 import { notFoundError } from "@/errors";
 import { cannotListHotelsError } from "@/errors/cannot-list-hotels-error";
+import { Hotel } from "@prisma/client";
+import roomRepository from "@/repositories/room-repository";
+import createText from "@/utils/hotels-utils";
+import { exclude } from "@/utils/prisma-utils";
 
 async function listHotels(userId: number) {
   //Tem enrollment?
@@ -17,12 +21,27 @@ async function listHotels(userId: number) {
     throw cannotListHotelsError();
   }
 }
-
+type HotelsWithRooms = Hotel & { availability?:number , accommodationType?:string}
 async function getHotels(userId: number) {
-  await listHotels(userId);
-
-  const hotels = await hotelRepository.findHotels();
-  return hotels;
+  //await listHotels(userId);//TODO:UNCOMMENT
+  const hotels:HotelsWithRooms[] = await hotelRepository.findHotels();
+  await Promise.all(
+    hotels.map(async (o, i) => {
+      const bookings = await hotelRepository.findBooking(o.id)
+      hotels[i].availability = await hotelRepository.findHotel(o.id) - bookings.reduce((acc, { _count: { Booking: booking } }) => acc + booking, 0)
+      const roomsCapacity = await roomRepository.findAllByHotelId(o.id)
+      const single = roomsCapacity.some((o,i)=>o.capacity===1)
+      const double = roomsCapacity.some((o,i)=>o.capacity===2)
+      const triple = roomsCapacity.some((o,i)=>o.capacity===3)
+      const newArray:string[] = []
+      if(single) newArray.push("Single")
+      if(double) newArray.push("Double")
+      if(triple) newArray.push("Triple")
+      hotels[i].accommodationType = createText(newArray)
+      //exclude(hotels[i], "createdAt", "updatedAt")
+    })
+  )
+  return hotels
 }
 
 async function getHotelsWithRooms(userId: number, hotelId: number) {
