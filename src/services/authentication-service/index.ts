fc986ce9563a648ddc06b7d16ba2af456747ad3e
Larissa <larissaalves1102@gmail.com>
2023-08-08 14:20:55 -0700

    feat signInWithGitHub: login system with GitHub combined with email/password

--- src/services/authentication-service/index.ts
+++ src/services/authentication-service/index.ts
@@ -5,6 +5,8 @@ import { User } from "@prisma/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { invalidCredentialsError } from "./errors";
+import axios from "axios";
+import qs from "query-string";
 
 async function signIn(params: SignInParams): Promise<SignInResult> {
   const { email, password } = params;
@@ -19,6 +21,72 @@ async function signIn(params: SignInParams): Promise<SignInResult> {
     user: exclude(user, "password"),
     token,
   };
+}
+
+async function signInWithGitHub(code: string){
+  const token = await exchangeCodeForAccessToken(code);
+  return token;
+}
+
+async function exchangeCodeForAccessToken(code:string) {
+  const GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+
+  const { REDIRECT_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
+  const params: GitHubParamsForAccessToken = {
+    code,
+    grant_type: "authorization_code",
+    redirect_uri: REDIRECT_URL,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  }
+
+  const { data } = await axios.post(GITHUB_ACCESS_TOKEN_URL, params, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const { access_token } = qs.parse(data);
+  const tokenGitHub = Array.isArray(access_token) ? access_token.join("") : access_token;
+
+  const userGitHub = await fetchUserFromGitHub(tokenGitHub);
+
+  let user = await userRepository.findById(userGitHub.id, { id: true, email: true, password: true });
+  if (!user) {
+    const data = {
+      id: userGitHub.id,
+      email: userGitHub.email ? userGitHub.email : 'null',
+      password: userGitHub.node_id
+    }
+    user = await userRepository.create(data);
+  }
+
+  const token = await createSession(user.id);
+
+  return {
+    user: exclude(user, "password"),
+    token,
+  };
+  
+}
+
+type GitHubParamsForAccessToken = {
+  code: string;
+  grant_type: string;
+  redirect_uri: string;
+  client_id: string;
+  client_secret: string;
+}
+
+export async function fetchUserFromGitHub(token: string) {
+  const GITHUB_USER_URL = "https://api.github.com/user";
+  const response = await axios.get(GITHUB_USER_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data;
 }
 
 async function getUserOrFail(email: string): Promise<GetUserOrFailResult> {
@@ -54,6 +122,7 @@ type GetUserOrFailResult = Pick<User, "id" | "email" | "password">;
 
 const authenticationService = {
   signIn,
+  signInWithGitHub
 };
 
 export default authenticationService;
