b15fad4631ee5ed3eec2cc5241ca17dffb825363
Bianca Dias <bianca.dias@ufv.br>
2023-08-10 20:46:07 -0300

    fix: error handling

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -4,9 +4,23 @@ import { Response } from "express";
 import httpStatus from "http-status";
 
 export async function getActivities(req:AuthenticatedRequest, res:Response){
+  try{
     const {userId} = req    
     const activities = await activitiesService.getActivities(userId)
     return res.status(httpStatus.OK).send(activities)
+  }catch (error) {
+
+    if (error.name === 'NotFoundError') {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+    if (error.type === 'unpaid') {
+      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    }
+    if (error.type === 'unavailable') {
+      return res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
+    }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
 }
 
 export async function createUserActivity(req:AuthenticatedRequest, res:Response){
