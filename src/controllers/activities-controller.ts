4b406b4aedd483671ca022b4a21c6aaffcd96f2e
Larissa <larissaalves1102@gmail.com>
2023-08-10 17:04:04 -0700

    fix: error handler in POST/activities

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -3,30 +3,43 @@ import activitiesService from "@/services/activities-service";
 import { Response } from "express";
 import httpStatus from "http-status";
 
-export async function getActivities(req:AuthenticatedRequest, res:Response){
-  try{
-    const {userId} = req    
-    const activities = await activitiesService.getActivities(userId)
-    return res.status(httpStatus.OK).send(activities)
-  }catch (error) {
+export async function getActivities(req: AuthenticatedRequest, res: Response) {
+    try {
+        const { userId } = req
+        const activities = await activitiesService.getActivities(userId)
+        return res.status(httpStatus.OK).send(activities)
+    } catch (error) {
 
-    if (error.name === 'NotFoundError') {
-      return res.sendStatus(httpStatus.NOT_FOUND);
+        if (error.name === 'NotFoundError') {
+            return res.sendStatus(httpStatus.NOT_FOUND);
+        }
+        if (error.type === 'unpaid') {
+            return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+        }
+        if (error.type === 'unavailable') {
+            return res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
+        }
+        return res.sendStatus(httpStatus.BAD_REQUEST);
     }
-    if (error.type === 'unpaid') {
-      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
-    }
-    if (error.type === 'unavailable') {
-      return res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
-    }
-    return res.sendStatus(httpStatus.BAD_REQUEST);
-  }
 }
 
-export async function createUserActivity(req:AuthenticatedRequest, res:Response){
-    const {userId} = req;
-    const {activityId} = req.body; 
-    await activitiesService.createUserActivity(userId, activityId);
-    return res.status(httpStatus.OK).send('Vaga reservada com sucesso.');
+export async function createUserActivity(req: AuthenticatedRequest, res: Response) {
+    try {
+        const { userId } = req;
+        const { activityId } = req.body;
+        await activitiesService.createUserActivity(userId, activityId);
+        return res.status(httpStatus.OK).send('Vaga reservada com sucesso.');
+    } catch (err) {
+        if (err.name === "ConflictError") {
+            return res.status(httpStatus.CONFLICT).send({
+                message: err.message,
+            });
+        }
+        if (err.name === 'PaymentRequiredError') {
+            return res.status(httpStatus.PAYMENT_REQUIRED).send({
+              message: err.message,
+            });
+          }
+    }
 }
 
